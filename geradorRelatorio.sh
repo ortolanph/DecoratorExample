0fd3d17d13b1eb2dbaa71ebf54c80df3fcee4f2a
Paulo Henrique Ortolan <paulo.ortolan@serpro.gov.br>
2018-10-01 15:06:48 -0300

    Documentos

---
+++ geradorRelatorio.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WD=$PWD
+
+function gerarRelatorio() {
+	cd $1
+	[ -f "$2.pdf" ] && rm $2.pdf
+	pandoc -f markdown_github -t pdf -t latex -o $2.pdf $2.md
+	cd $WD
+}
+
+gerarRelatorio diagramas exercicios
+gerarRelatorio questionario questionario
+gerarRelatorio documentos palavras-chave-heranca
